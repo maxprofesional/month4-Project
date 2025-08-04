6c1840d01cddad872f5e38b306e178909810ab84
maxprofesional <movodoeva@gmail.com>
2025-08-04 18:17:48 +0600

    first commit

--- js/home_works.js
+++ js/home_works.js
@@ -1,20 +1,41 @@
-// MOVE BLOCK HW 1.2
+//REG EXP part 1
+const gmailBtn = document.querySelector("#gmail_button");
+const gmailInp = document.querySelector("#gmail_input");
+const gmailResult = document.querySelector("#gmail_result");
 
-const parentBlock = document.querySelector('.parent_block');
-const childBlock = document.querySelector('.child_block');
+const rgx = /^[a-zA-Z][a-zA-Z0-9]{3,15}@gmail\.com$/;
 
-let positionX = 0
-let positionY = 0
+gmailBtn.addEventListener("click", () => {
+  const text = gmailInp.value;
 
-const offWidth = parentBlock.offsetWidth - childBlock.offsetWidth
-const offHeight = parentBlock.offsetHeight - childBlock.offsetHeight
+  if (rgx.test(text)) {
+    gmailResult.textContent = "Валидный адрес";
+    gmailResult.style.color = "green";
+  } else {
+    gmailResult.textContent = "Не валидный адрес";
+    gmailResult.style.color = "red";
+  }
+});
 
-const moveBlock = () => {
-    if (positionX < offWidth) positionX++
-    if (positionX >= offWidth && positionY < offHeight) positionY++
-    childBlock.style.left = `${positionX}px`
-    childBlock.style.top = `${positionY}px`
-    requestAnimationFrame(moveBlock)
+//part 2
+const parentBlock = document.querySelector(".parent_block");
+const childBlock = document.querySelector(".child_block");
+
+const width = Number(parentBlock.offsetWidth) - Number(childBlock.offsetWidth);
+function MoveBlockRight(pixel = 0) {
+  if (pixel < width) {
+    childBlock.style.left = `${pixel}px`;
+    requestAnimationFrame(() => MoveBlockRight(++pixel));
+  } else {
+    MoveBlockLeft();
+  }
 }
-
-moveBlock();
+function MoveBlockLeft(pixel = width) {
+  if (pixel > 0) {
+    childBlock.style.left = `${pixel}px`;
+    requestAnimationFrame(() => MoveBlockLeft(--pixel));
+  } else {
+    MoveBlockRight();
+  }
+}
+MoveBlockRight();
