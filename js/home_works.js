95e173b2860c48003b97b359c6ac0f0b9d69ed6e
maxprofesional <movodoeva@gmail.com>
2025-08-07 17:50:41 +0600

    homeWork #2

--- js/home_works.js
+++ js/home_works.js
@@ -3,7 +3,7 @@ const gmailBtn = document.querySelector("#gmail_button");
 const gmailInp = document.querySelector("#gmail_input");
 const gmailResult = document.querySelector("#gmail_result");
 
-const rgx = /^[a-zA-Z][a-zA-Z0-9]{3,15}@gmail\.com$/;
+const rgx = /^[a-zA-Z] \w{3,15}@gmail\.com$/;
 
 gmailBtn.addEventListener("click", () => {
   const text = gmailInp.value;
@@ -21,21 +21,53 @@ gmailBtn.addEventListener("click", () => {
 const parentBlock = document.querySelector(".parent_block");
 const childBlock = document.querySelector(".child_block");
 
-const width = Number(parentBlock.offsetWidth) - Number(childBlock.offsetWidth);
-function MoveBlockRight(pixel = 0) {
-  if (pixel < width) {
-    childBlock.style.left = `${pixel}px`;
-    requestAnimationFrame(() => MoveBlockRight(++pixel));
-  } else {
-    MoveBlockLeft();
+const width = parentBlock.clientWidth - childBlock.clientWidth;
+const height = parentBlock.clientHeight - childBlock.clientHeight;
+let posX = 0;
+let posY = 0;
+function MoveBlock() {
+  childBlock.style.left = `${posX}px`;
+  childBlock.style.top = `${posY}px`;
+
+  if (posX < width && posY === 0) {
+    posX++;
+  } else if (posX === width && posY < height) {
+    posY++;
+  } else if (posY === height && posX > 0) {
+    posX--;
+  } else if (posX === 0 && posY > 0) {
+    posY--;
   }
+  requestAnimationFrame(() => MoveBlock());
 }
-function MoveBlockLeft(pixel = width) {
-  if (pixel > 0) {
-    childBlock.style.left = `${pixel}px`;
-    requestAnimationFrame(() => MoveBlockLeft(--pixel));
-  } else {
-    MoveBlockRight();
-  }
+
+MoveBlock();
+
+//Timer HomeWork 2
+
+const resultTimerBlock = document.querySelector("#seconds");
+const startBtn = document.querySelector("#start");
+const stopBtn = document.querySelector("#stop");
+const resetBtn = document.querySelector("#reset");
+
+let interval = null;
+let count = 0;
+
+startBtn.onclick = () => {
+  if (interval === null) interval = setInterval(timer, 1000);
+};
+resetBtn.onclick = () => {
+  clearInterval(interval);
+  count = 0;
+  resultTimerBlock.textContent = 0;
+  interval = null;
+};
+stopBtn.onclick = () => {
+  clearInterval(interval);
+  interval = null;
+};
+
+function timer() {
+  count++;
+  resultTimerBlock.textContent = count;
 }
-MoveBlockRight();
